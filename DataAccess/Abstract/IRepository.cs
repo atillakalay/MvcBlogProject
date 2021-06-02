5a4235a819a8b5b61e8216766368eea86cb19146
atilla kalay <atilla.kalayy@gmail.com>
2021-06-02 16:25:09 +0300

    DataAccess' codes added

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        int Add(T entity);
+        int Update(T entity);
+        int Delete(T entity);
+        T GetById(int id);
+
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
